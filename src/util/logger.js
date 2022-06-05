9c9dca205b4cc46f577e8cbec8539c45f2b897a6
harsh <hvbadeiya24@gmail.com>
2022-06-05 19:57:15 +0530

    Utilities

---
+++ src/util/logger.js
@@ -0,0 +1,17 @@
+import logger from 'pino';
+import PinoPretty from 'pino-pretty';
+
+const log = logger({
+ 
+    base:{pid:false},
+    transport:{
+        target:'pino-pretty',
+        options:{
+            colorized: true
+        }
+    },
+    timestamp:()=>`,"time": "${new Date().toLocaleString()}"`
+
+});
+
+export default log;
